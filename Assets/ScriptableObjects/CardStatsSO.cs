ed8b0842c12148fd106c5b02f09d72bb86b79153
Reggie <jacobdyak@gmail.com>
2023-11-04 14:03:49 -0700

    Added SO for card stats

---
+++ Assets/ScriptableObjects/CardStatsSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CardStatsSO : ScriptableObject
+{
+    public float defaultHealth;
+    public float defaultAttack;
+    public float defaultDefense;
+    public float defaultSpeed;
+    public float turnsAlive;
+}
