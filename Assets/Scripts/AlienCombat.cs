2bde81a5ca8898dd5f0dcf6b219bc540c7e0a2f1
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2023-11-14 12:50:38 -0800

    Create combat scene
    This is just the changes I did in the meeting.

---
+++ Assets/Scripts/AlienCombat.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienCombat : MonoBehaviour
+{
+    CardDataSO alienData;
+
+    int damageTaken;
+    int turnsTaken;
+
+    
+}
